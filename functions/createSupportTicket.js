ffe531a0487594ae756f0c9a501fdfcef370d440
Emissions <2356927-Emissions@users.noreply.gitlab.com>
2019-05-23 09:51:33 +0000

    Update createSupportTicket.js

--- functions/createSupportTicket.js
+++ functions/createSupportTicket.js
@@ -1,7 +1,18 @@
-let createSupportTicketFunction = function createSupportTicket(guild, author, member) {
+let createSupportTicketFunction = function createSupportTicket(guild, author, member, channel, client) {
     let random_string = require(`crypto-random-string`);
     guild.createChannel(`support-${author.username}-${random_string({length: 10, type: 'base64'})}`).then(async(channel) => {
-        return;
+        let category = guild.channels.find(c => c.name == "Support Tickets" && c.type == "category");
+        if (!category) {
+            let embed = new client.modules.Discord.MessageEmbed()
+                .setTitle(`**Support Ticket** - Error`)
+                .setColor(guild.member(client.user).displayHexColor)
+                .setDescription(`**Hey!** Unfortunately, we could not create your support ticket! Please display this error code to a member of staff.`)
+                .addField(`Error Code`, `s001`)
+                .setTimestamp();
+            channel.send(embed);
+        } else {
+            return;
+        }
     });
 }
 
