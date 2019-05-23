b464f1022e04ba290886bdb6feb816ebc1b393db
Emissions <2356927-Emissions@users.noreply.gitlab.com>
2019-05-23 09:54:17 +0000

    Update errorcode.js

--- commands/errorcode.js
+++ commands/errorcode.js
@@ -1,3 +1,11 @@
 module.exports = class errorcode {
+    constructor() {
+        this.name = 'errorcode',
+            this.alias = ["error", "errcode"],
+            this.usage = `-errorcode`
+    }
 
+    async run(client, message, args) {
+        let ec = client.storage.errorCodes;
+    }
 }
