027947cfdf17e8d0ee88c8b33f6cdb868cb37371
Emissions <2356927-Emissions@users.noreply.gitlab.com>
2019-05-23 09:40:19 +0000

    Update createSupportTicket.js

--- functions/createSupportTicket.js
+++ functions/createSupportTicket.js
@@ -1,5 +1,8 @@
-let createSupportTicketFunction = function createSupportTicket() {
-
+let createSupportTicketFunction = function createSupportTicket(guild, author, member) {
+    let random_string = require(`crypto-random-string`);
+    guild.createChannel(`support-${author.username}-${random_string({length: 10, type: 'base64'})}`).then(async(channel) => {
+        return;
+    });
 }
 
 module.exports = createSupportTicketFunction;
