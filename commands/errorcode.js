435d672edcda221b69591ba365d90cd60f9e204c
Emissions <2356927-Emissions@users.noreply.gitlab.com>
2019-05-23 09:59:49 +0000

    Update errorcode.js

--- commands/errorcode.js
+++ commands/errorcode.js
@@ -7,5 +7,18 @@ module.exports = class errorcode {
 
     async run(client, message, args) {
         let ec = client.storage.errorCodes;
+        if (!ec[`${args[1].toString()}`]) {
+            let embed = new client.modules.Discord.MessageEmbed()
+                .setTitle(`**Error Codes** - ${args[1].toString()}`)
+                .setColor(message.guild.member(client.user).displayHexColor)
+                .setDescription(`Unfortunately, we could not find the Error Code ` + "`" + args[1].toString() + "`" + `. Please make sure your Error Code is exactly as written.`)
+            message.channel.send(embed);
+        } else {
+            let embed = new client.modules.Discord.MessageEmbed()
+                .setTitle(`**Error Codes** - ${args[1].toString()}`)
+                .setColor(message.guild.member(client.user).displayHexColor)
+                .setDescription(ec[`${args[1].toString()}`])
+            message.channel.send(embed);
+        }
     }
 }
