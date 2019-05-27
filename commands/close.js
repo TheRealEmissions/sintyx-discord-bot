ddd6e0a77091571ec66c8de2e60654c643afb44b
Emissions <unitedrealm@hotmail.com>
2019-05-27 17:43:39 +0100

    Added titles to the embeds on -close

--- commands/close.js
+++ commands/close.js
@@ -12,16 +12,18 @@ module.exports = class close {
         } else {
             let reason = Boolean(message.content.slice(args[0].length + 1)) ? message.content.slice(args[0].length + 1) : `No reason provided`;
             let confirmClose = new client.modules.Discord.MessageEmbed()
+                .setTitle(`**Support Ticket** - Closing`)
                 .setDescription(`**Are you sure you want to close this ticket?** If so, please confirm below.`)
                 .addField("Reason", "```" + reason + "```")
                 .setColor(message.guild.member(client.user).displayHexColor)
-            message.channel.send(confirmClose).then(async(msg) => {
+            message.channel.send(confirmClose).then(async (msg) => {
                 await msg.react(client.storage.emojiCharacters['white_check_mark']);
                 let filter = (reaction, user) => reaction.emoji.name == client.storage.emojiCharacters['white_check_mark'] && user.id == message.author.id;
                 let collector = new client.modules.Discord.ReactionCollector(msg, filter, {});
-                collector.on('collect', async(reaction) => {
+                collector.on('collect', async (reaction) => {
                     collector.stop();
                     let confirmed = new client.modules.Discord.MessageEmbed()
+                        .setTitle(`**Support Ticket** - Closing`)
                         .setDescription(`Thank you for seeking support and opening this ticket. We hope the issue you had has been resolved. If not, please be sure to open another ticket!`)
                         .addField("Reason", "```" + reason + "```")
                         .setColor(message.guild.member(client.user).displayHexColor)
