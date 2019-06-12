22306cc0641ae221804c7ee46faa3ab9195bfb65
Emissions <unitedrealm@hotmail.com>
2019-06-12 17:48:20 +0100

    Updated models.js with supportTickets

--- models.js
+++ models.js
@@ -1,5 +1,6 @@
 module.exports = {
     userSettings: require(`./models/userSettings.js`),
     trelloCards: require(`./models/trelloCards.js`),
-    userProfiles: require(`./models/userProfiles.js`)
+    userProfiles: require(`./models/userProfiles.js`),
+    supportTickets: require(`./models/supportTickets.js`)
 }
