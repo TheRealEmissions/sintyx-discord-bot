fa77a08b97db14e74161f35e89ae4d0fd69e815e
Emissions <unitedrealm@hotmail.com>
2019-07-10 18:29:53 +0100

    Updated -ticket and removed the requirement for the "info" args[1]

--- commands/ticket.js
+++ commands/ticket.js
@@ -20,42 +20,41 @@ module.exports = class ticket {
 
 
         */
-        if (!args[1]) return;
-        if (args[1].toString() == "info") {
-            if (!args[2]) return;
-            client.models.supportTickets.findOne({
-                "reference_id": args[2]
-            }, (err, db) => {
-                if (err) return console.error(err);
-                if (!db) {
-                    return message.channel.send(`The reference ID you have provided does not match a support ticket.`);
-                }
-                if ((message.member.roles.find(x => x.name == "Owner")) || (db.user_id == message.author.id)) {
-                    let embed;
-                    client.modules.fs.appendFileSync(`./commands/${db.channel_id}.json`, JSON.stringify(db.logs));
-                    if (db.closure_id == null) {
-                        embed = new client.modules.Discord.MessageEmbed()
-                            .setTitle(`Ticket Information - **${args[2]}**`)
-                            .setColor(message.guild.member(client.user).displayHexColor)
-                            .addField(`Opened by:`, `<@${message.guild.members.find(x => x.id == db.user_id).id}>`, true)
-                            .addField(`Reason for opening:`, "```" + db.channel_reason + "```", true)
-                    } else {
-                        embed = new client.modules.Discord.MessageEmbed()
-                            .setTitle(`Ticket Information - **${args[2]}**`)
-                            .setColor(message.guild.member(client.user).displayHexColor)
-                            .addField(`Opened by:`, `<@${message.guild.members.find(x => x.id == db.user_id).id}>`, true)
-                            .addField(`Reason for opening:`, "```" + db.channel_reason + "```", true)
-                            .addField(`Closed by:`, `<@${message.guild.members.find(x => x.id == db.closure_id).id}>`, true)
-                            .addField(`Closure reason:`, "```" + db.closure_reason + "```", true)
-                    }
-                    message.channel.send(embed);
-                    message.channel.send({files: [`./commands/${db.channel_id}.json`]}).then(() => {
-                        client.modules.fs.unlink(`./commands/${db.channel_id}.json`, (err) => console.error(err));
-                    });
+        if (!args[2]) return;
+        client.models.supportTickets.findOne({
+            "reference_id": args[2]
+        }, (err, db) => {
+            if (err) return console.error(err);
+            if (!db) {
+                return message.channel.send(`The reference ID you have provided does not match a support ticket.`);
+            }
+            if ((message.member.roles.find(x => x.name == "Owner")) || (db.user_id == message.author.id)) {
+                let embed;
+                client.modules.fs.appendFileSync(`./commands/${db.channel_id}.json`, JSON.stringify(db.logs));
+                if (db.closure_id == null) {
+                    embed = new client.modules.Discord.MessageEmbed()
+                        .setTitle(`Ticket Information - **${args[2]}**`)
+                        .setColor(message.guild.member(client.user).displayHexColor)
+                        .addField(`Opened by:`, `<@${message.guild.members.find(x => x.id == db.user_id).id}>`, true)
+                        .addField(`Reason for opening:`, "```" + db.channel_reason + "```", true)
                 } else {
-                    return message.channel.send(`You do not have permission to view the information for this ticket.`);
+                    embed = new client.modules.Discord.MessageEmbed()
+                        .setTitle(`Ticket Information - **${args[2]}**`)
+                        .setColor(message.guild.member(client.user).displayHexColor)
+                        .addField(`Opened by:`, `<@${message.guild.members.find(x => x.id == db.user_id).id}>`, true)
+                        .addField(`Reason for opening:`, "```" + db.channel_reason + "```", true)
+                        .addField(`Closed by:`, `<@${message.guild.members.find(x => x.id == db.closure_id).id}>`, true)
+                        .addField(`Closure reason:`, "```" + db.closure_reason + "```", true)
                 }
-            })
-        }
+                message.channel.send(embed);
+                message.channel.send({
+                    files: [`./commands/${db.channel_id}.json`]
+                }).then(() => {
+                    client.modules.fs.unlink(`./commands/${db.channel_id}.json`, (err) => console.error(err));
+                });
+            } else {
+                return message.channel.send(`You do not have permission to view the information for this ticket.`);
+            }
+        });
     }
 }
