e98e75f6e9b4362285b09079767c6a82211e8524
Emissions <unitedrealm@hotmail.com>
2019-07-11 11:06:58 +0100

    Added -mentionable <@role/role ID>

---
+++ commands/mentionable.js
@@ -0,0 +1,23 @@
+module.exports = class mentionable {
+    constructor() {
+        this.name = 'mentionable',
+            this.alias = [],
+            this.usage = '-mentionable',
+            this.category = 'administration',
+            this.description = 'Toggle making a role mentionable on/off'
+    }
+
+    async run(client, message, args) {
+        if (message.member.roles.find(x => x.name == "Owner")) {
+            let role = Boolean(message.mentions.roles.first()) ? message.mentions.roles.first() : message.guild.roles.get(`${args[1]}`);
+            if (!role) {
+                return message.channel.send(`Please either mention a role or type its ID.`).then(msg => setTimeout(() => msg.delete(), 5000));
+            }
+            role.edit({
+                mentionable: role.mentionable ? false : true
+            }).then(() => {
+                message.channel.send(`Altered mentionability of **${role.name}** to **${role.mentionable ? 'true' : 'false'}**`);
+            });
+        }
+    }
+}
