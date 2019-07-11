d48c04b122423b22000265d9e9c0edb0d4c3d09b
Emissions <unitedrealm@hotmail.com>
2019-07-11 12:28:58 +0100

    Added -purge <number>

---
+++ commands/purge.js
@@ -0,0 +1,22 @@
+module.exports = class purge {
+    constructor() {
+        this.name = 'purge',
+        this.alias = [],
+        this.usage = '-purge <number>',
+        this.category = 'administration',
+        this.description = 'Delete a number of messages from a channel (limit 1000)'
+    }
+
+    async run(client, message, args) {
+        if (message.member.roles.find(x => x.name == "Owner")) {
+            if (args[1] <= 1000) {
+                message.channel.bulkDelete(args[1]).then(messages => {
+                    let embed = new client.modules.Discord.MessageEmbed()
+                        .setColor(message.guild.member(client.user).displayHexColor)
+                        .setDescription(`Deleted ${messages.size} messages from ${message.channel}`)
+                    message.channel.send(embed);
+                });
+            }
+        }
+    }
+}
