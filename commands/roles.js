d1eb8c6656b6afc62e3dfdb71f6b523dbe85bbac
Emissions <unitedrealm@hotmail.com>
2019-07-11 10:59:43 +0100

    Added -roles

---
+++ commands/roles.js
@@ -0,0 +1,24 @@
+module.exports = class roles {
+    constructor() {
+        this.name = 'roles',
+        this.alias = ["rolelist"],
+        this.usage = '-roles',
+        this.category = 'misc',
+        this.description = 'View a list of all roles and its member count'
+    }
+
+    async run(client, message, args) {
+        let roles = []
+        message.guild.roles.filter(r => r.name !== "@everyone").forEach(role => {
+            roles.push({
+                id: role.id,
+                member_count: role.members.size
+            });
+        });
+        let embed = new client.modules.Discord.MessageEmbed()
+            .setColor(message.guild.member(client.user).displayHexColor)
+            .addField(`Role`, roles.map(r => `<@&${r.id}>`).join(`\n`), true)
+            .addField(`Member Count`, roles.map(r => `${r.member_count}`).join(`\n`), true)
+        message.channel.send(embed);
+    }
+}
