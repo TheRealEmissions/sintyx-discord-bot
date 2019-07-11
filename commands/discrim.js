5385b6afc7cae3886f7436c1c5bae6d3e4238f33
Emissions <unitedrealm@hotmail.com>
2019-07-11 12:36:54 +0100

    Added -discrim

---
+++ commands/discrim.js
@@ -0,0 +1,26 @@
+module.exports = class discrim {
+    constructor() {
+        this.name = 'discrim',
+        this.alias = ['discriminator'],
+        this.usage = '-discrim <4 digit number>',
+        this.category = 'misc',
+        this.description = 'Get a list of users with a specific discriminator'
+    }
+
+    async run(client, message, args) {
+        if (args[1].length == 4) {
+            let obj = []
+            message.guild.members.forEach(member => {
+                if (member.user.discriminator == args[1]) {
+                    obj.push({
+                        id: member.user.id
+                    });
+                }
+            });
+            let embed = new client.modules.Discord.MessageEmbed()
+                .setColor(message.guild.member(client.user).displayHexColor)
+                .addField(`Users with the discriminator **${args[1]}**:`, obj.map(r => `<@${r.id}>`).join(`\n`))
+            message.channel.send(embed);
+        }
+    }
+}
