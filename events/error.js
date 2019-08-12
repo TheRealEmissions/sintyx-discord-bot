e641b1fd2723600ee9e41fd9ee458e1d2d4eabaf
Emissions <unitedrealm@hotmail.com>
2019-08-12 12:37:15 +0100

    Fixed error.js (event) so it now actually logs errors properly in Discord

--- events/error.js
+++ events/error.js
@@ -2,7 +2,7 @@ module.exports = (client, error) => {
     let Discord = require(`discord.js`);
     let guild = client.guilds.find(x => x.id == "567439300543905821");
     let channel = guild.channels.find(x => x.id == "581508137454927883");
-    let embed = Discord.MessageEmbed()
+    let embed = new Discord.MessageEmbed()
         .setTitle(`**Error!**`)
         .setDescription("```" + client.functions.trim(error, 1000) + "```")
         .setColor(guild.member(client.user).displayHexColor)
