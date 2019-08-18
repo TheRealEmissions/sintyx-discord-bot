a7944621a9d16bd4d78071aef5fd7bf0543eded3
Emissions <unitedrealm@hotmail.com>
2019-08-18 23:45:40 +0100

    Updated suggestionaccept.js (command) to support Sintyx roles

--- commands/suggestionaccept.js
+++ commands/suggestionaccept.js
@@ -8,7 +8,7 @@ module.exports = class suggestionaccept {
     }
 
     async run(client, message, args) {
-        if (!message.member.roles.get(message.guild.roles.find(x => x.name == "The Lord").id)) return;
+        if (!message.member.roles.get(message.guild.roles.find(x => x.name == "Owner").id)) return;
         if (!args[1]) return;
         client.models.suggestionsData.findOne({
             "reference_id": args[1]
