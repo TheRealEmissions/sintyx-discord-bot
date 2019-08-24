133a48f071d9dcd37a3c4af4996a5c0885f42497
Emissions <unitedrealm@hotmail.com>
2019-08-24 14:06:02 +0100

    Removed ticket.js (command)
    Updated tickets.js (command)

--- commands/tickets.js
+++ commands/tickets.js
@@ -2,7 +2,7 @@ module.exports = class tickets {
     constructor() {
         this.name = 'tickets',
             this.usage = '-tickets',
-            this.alias = ["tickethistory", "thistory"],
+            this.alias = ["tickethistory", "thistory", "ticket"],
             this.category = 'user',
             this.description = 'Shows a list of your previous Support Ticket reference IDs'
     }
@@ -11,7 +11,7 @@ module.exports = class tickets {
         if (!args[1]) {
             client.models.userProfiles.find({
                 "user_id": message.author.id
-            }).lean().exec(async(err, docs) => {
+            }).lean().exec(async (err, docs) => {
                 if (err) return new client.methods.log(client, message.guild).error(err);
 
                 function replaceMonth(term) {
@@ -60,7 +60,8 @@ module.exports = class tickets {
                     message.channel.send(`You have not opened any tickets! Please try opening a ticket before running this command.`)
                 }
             });
-        } else {
+        }
+        if (message.mentions.users.first() && !args[2]) {
             if (message.member.roles.find(x => x.name == "Management")) {
                 let user = Boolean(message.mentions.users.first()) ? message.mentions.users.first() : await Promise.resolve(client.users.fetch(args[1]));
                 if (user) {
@@ -120,6 +121,69 @@ module.exports = class tickets {
                     message.channel.send(`Unfortunately, I couldn't find that user. Please either ping that user or type their ID after the command.`)
                 }
             }
+        }
+        if (!message.mentions.users.first() && !args[2] && args[1]) {
+            client.models.supportTickets.findOne({
+                "reference_id": args[1]
+            }, (err, db) => {
+                if (err) return new client.methods.log(client, message.guild).error(err);
+                if (!db) return message.channel.send(`The reference ID you have provided does not match a support ticket.`);
+                if ((message.member.roles.find(x => x.name == "Management")) || (db.user_id == message.author.id)) {
+                    let embed;
+                    client.modules.fs.appendFileSync(`./commands/${db.channel_id}.json`, JSON.stringify(db.logs));
+                    if (db.closure_id == null) {
+                        embed = new client.modules.Discord.MessageEmbed()
+                            .setTitle(`Ticket Information - **${args[2]}**`)
+                            .setColor(message.guild.member(client.user).displayHexColor)
+                            .addField(`Opened by:`, `<@${message.guild.members.find(x => x.id == db.user_id).id}>`, true)
+                            .addField(`Reason for opening:`, "```" + db.channel_reason + "```", true)
+                    } else embed = new client.modules.Discord.MessageEmbed()
+                        .setTitle(`Ticket Information - **${args[2]}**`)
+                        .setColor(message.guild.member(client.user).displayHexColor)
+                        .addField(`Opened by:`, `<@${message.guild.members.find(x => x.id == db.user_id).id}>`, true)
+                        .addField(`Reason for opening:`, "```" + db.channel_reason + "```", true)
+                        .addField(`Closed by:`, `<@${message.guild.members.find(x => x.id == db.closure_id).id}>`, true)
+                        .addField(`Closure reason:`, "```" + db.closure_reason + "```", true)
+                    message.channel.send(embed);
+                    message.channel.send({
+                        files: [`./commands/${db.channel_id}.json`]
+                    }).then(() => {
+                        client.modules.fs.unlink(`./commands/${db.channel_id}.json`, (err) => new client.methods.log(client, message.guild).error(err))
+                    });
+                } else return message.channel.send(`You do not have permission to view the information for this ticket!`);
+            });
+        }
+        if (message.mentions.users.first() && args[2]) {
+            if (!message.member.roles.find(x => x.name == "Management")) return;
+            let user = message.mentions.users.first();
+            if (!user) return;
+            client.models.supportTickets.findOne({
+                "reference_id": args[2]
+            }, (err, db) => {
+                if (err) return new client.methods.log(client).error(err);
+                if (!db) return message.channel.send(`The reference ID you have provided does not match a support ticket.`);
+                let embed;
+                client.modules.fs.appendFileSync(`./commands/${db.channel_id}.json`, JSON.stringify(db.logs));
+                if (db.closure_id == null) {
+                    embed = new client.modules.Discord.MessageEmbed()
+                        .setTitle(`Ticket Information - **${args[2]}**`)
+                        .setColor(message.guild.member(client.user).displayHexColor)
+                        .addField(`Opened by:`, `<@${message.guild.members.find(x => x.id == db.user_id).id}>`, true)
+                        .addField(`Reason for opening:`, "```" + db.channel_reason + "```", true)
+                } else embed = new client.modules.Discord.MessageEmbed()
+                    .setTitle(`Ticket Information - **${args[2]}**`)
+                    .setColor(message.guild.member(client.user).displayHexColor)
+                    .addField(`Opened by:`, `<@${message.guild.members.find(x => x.id == db.user_id).id}>`, true)
+                    .addField(`Reason for opening:`, "```" + db.channel_reason + "```", true)
+                    .addField(`Closed by:`, `<@${message.guild.members.find(x => x.id == db.closure_id).id}>`, true)
+                    .addField(`Closure reason:`, "```" + db.closure_reason + "```", true)
+                message.channel.send(embed);
+                message.channel.send({
+                    files: [`./commands/${db.channel_id}.json`]
+                }).then(() => {
+                    client.modules.fs.unlink(`./commands/${db.channel_id}.json`, (err) => new client.methods.log(client).error(err));
+                });
+            });
         }
     }
 }
