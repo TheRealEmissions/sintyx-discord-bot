44532bc14ade3bba4d937589a79502f4909ac09b
Emissions <unitedrealm@hotmail.com>
2019-09-06 21:50:15 +0100

    Updated setslogan.js (command)

--- commands/setslogan.js
+++ commands/setslogan.js
@@ -2,12 +2,12 @@ module.exports = class setslogan {
     constructor() {
         this.name = 'setslogan',
             this.alias = [],
-            this.usage = 'setslogan <slogan>',
+            this.usage = '-setslogan <slogan>',
             this.category = 'user',
             this.description = 'Set your personal slogan for your profile!'
     }
 
-    checkFilter(term, filter, message) {
+    checkFilter(term, filter, message, client) {
         return new Promise((resolve, reject) => {
             term = term.toLowerCase().split(" ").join("");
             for (const word of filter) {
@@ -26,11 +26,12 @@ module.exports = class setslogan {
     async run(client, message, args) {
         message.delete();
         if (!args[1]) return;
-        let slogan = message.content.slice(args[0].length + 1);
+        let slogan = client.modules.Discord.escapeMarkdown(message.content.slice(args[0].length + 1));
         if (slogan.length > 150) return message.channel.send(`:x: Your slogan must be 150 characters or below!`).then((msg) => setTimeout(() => msg.delete(), 3000));
-        let filter = ["http://", "https://", ".com", ".org", ".net", ".us", ".edu", ".info", ".in", ".it", ".biz", ".co", ".one", ".name", ".uk", ".gov", ".mil", ".is", ".eu", ".de", ".pro", ".xyz", ".example", ".fr", ".aero", ".mx", ".mobi", ".au", ".io", ".ca", ".arpa", ".ru", ".es", ".tel", ".jobs", ".jp", ".asia", ".museum", ".tv", ".nl", ".ly", ".cn", ".top", ".int", ".ch", ".se", ".ws", ".eg", ".wiki", ".br", ".th", ".tk", ".cc", ".site", "www.", "porn", ".app", ".online", ".space", ".house", ".store", ".tech", ".club", "hypixel", "mineplex", "fragglecraft", "cosmicprisons", "cosmicprison", ".gg", ".blog", ".bz", ".ly", ".club", ".rewards", ".me", "arkhamnetwork", "hypxl", ".win", "hypixl", "hypxel", "hipixel"];
-        let check = await this.checkFilter(slogan, filter, message);
+        let filter = ["http://", "https://", ".com", ".org", ".net", ".us", ".edu", ".info", ".in", ".it", ".biz", ".co", ".one", ".name", ".uk", ".gov", ".mil", ".is", ".eu", ".de", ".pro", ".xyz", ".example", ".fr", ".aero", ".mx", ".mobi", ".au", ".io", ".ca", ".arpa", ".ru", ".es", ".tel", ".jobs", ".jp", ".asia", ".museum", ".tv", ".nl", ".ly", ".cn", ".top", ".int", ".ch", ".se", ".ws", ".eg", ".wiki", ".br", ".th", ".tk", ".cc", ".site", "www.", "porn", ".app", ".online", ".space", ".house", ".store", ".tech", ".club", "hypixel", "mineplex", "fragglecraft", "cosmicprisons", "cosmicprison", ".gg", ".blog", ".bz", ".ly", ".club", ".rewards", ".me", "arkhamnetwork", "hypxl", ".win", "hypixl", "hypxel", "hipixel", "nigger", "nigga", ".link", ".pw"];
+        let check = await this.checkFilter(slogan, filter, message, client);
         if (check == false) return;
+        slogan = slogan.filter(`\n`, '');
         client.models.userProfiles.findOne({
             "user_id": message.author.id
         }, (err, db) => {
