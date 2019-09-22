cbf1716b9b18fe643b21dec03d9be9857580f4df
Emissions <unitedrealm@hotmail.com>
2019-09-22 13:52:36 +0100

    Updated messageDelete.js (event) to check if the deletion author is a bot

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -1,5 +1,8 @@
 module.exports = async (client, message) => {
     if (message.guild.id == '564101344756236347') {
+        if (message.channel.type !== "text") return;
+        if (message.author.id == client.user.id) return;
+        if (message.author.bot) return;
         let channel = await client.channels.fetch(client.storage.messageCache['logChannel'].message_log_id);
         let embed = new client.modules.Discord.MessageEmbed()
             .setColor(message.guild.me.displayHexColor)
